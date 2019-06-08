91b30303953dcfe21d73d558d511ebf19877e6ce
Clever_Moon <qzr19970105@live.com>
2019-06-08 17:53:30 +0800

    💩 优化一些代码

---
+++ watero_go/tests/run.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+File : test.py
+Author : Zerui Qin
+CreateDate : 2019-05-22 23:48:29 
+Note : 
+"""
+
+import threading
+
+from watero_go.agent_client import AgentClient
+
+
+def bundle_send(p_data_service):
+    h_status = p_data_service.heartbeat()
+    r_status = p_data_service.resource()
+    timer = threading.Timer(60, bundle_send, args=[p_data_service, ])
+    timer.start()
+
+
+if __name__ == '__main__':
+    client = AgentClient('v1')
+    data_service = client.get_service('base_service')
+    data_service.auth()
+    bundle_send(data_service)
